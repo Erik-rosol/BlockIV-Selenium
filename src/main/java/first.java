d7bbca07a8a072e2d9a975edf10993a404b429c9
Erik-rosol <150236395+Erik-rosol@users.noreply.github.com>
2025-06-14 09:11:31 +0200

    Add files via upload

---
+++ src/main/java/first.java
@@ -0,0 +1,26 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class first {
+    public static void main(String[] args) {
+
+
+        System.setProperty("C:/Users/erik.vila/IdeaProjects/Selenium_Wakeb_training/drivers/chromedriver.exe","./drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+        try{
+            driver.get("https://www.wikipedia.es");
+            Thread.sleep(2000);
+
+
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            driver.quit();
+        }
+    }
+}
